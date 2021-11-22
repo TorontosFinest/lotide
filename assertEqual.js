b938df72480edbae7f5829b696e700367b267197
TorontosFinest <abdialicanada20@gmail.com>
2021-11-22 21:30:33 +0000

    linted file

--- assertEqual.js
+++ assertEqual.js
@@ -1,11 +1,9 @@
 const assertEqual = function(actual, expected) {
-    if(actual === expected){
-        console.log(`✅✅✅ Assertion passed.${actual} === ${expected}`)
-    }else{
-        console.log(`🛑🛑🛑 Assertion failed. ${actual} !== ${expected}`)
-    }
-    };
-    
-
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion passed.${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion failed. ${actual} !== ${expected}`);
+  }
+};
 assertEqual("Lighthouse Labs", "Bootcamp");
 assertEqual(1, 1);
