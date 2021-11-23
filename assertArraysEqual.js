c542486983e12c7c455adf3566b6502ab3ef05f5
TorontosFinest <abdialicanada20@gmail.com>
2021-11-23 23:52:38 +0000

    added assertArraysEqual.js

---
+++ assertArraysEqual.js
@@ -0,0 +1,18 @@
+var eqArrays = function(arr1, arr2) {
+
+    for (var i = 0; arr1.length < i; i++) {
+      if (arr1[i] !== arr2[i]) {
+        return false;
+      }
+    }
+    return true;
+  };
+
+  const assertArraysEqual = function(array1, array2) {
+    if (eqArrays(array1, array2)) {
+      console.log(`✅✅✅  Assertion passed: ${array1} === ${array2}`);
+    } else {
+      console.log(`🛑🛑🛑  Assertion failed: ${array1} !== ${array2}`);
+    }
+  };
+  
