ed56567da94e5970e80514d6f9c311d688cb742f
TorontosFinest <abdialicanada20@gmail.com>
2021-11-24 00:20:54 +0000

    added without.js

---
+++ without.js
@@ -0,0 +1,11 @@
+function without(source,remove){
+
+    for(let i = 0; i<source.length; i++){
+        for( let j=0; j< remove.length; j++){
+            if(source[i]===remove[j]){
+                source.splice(i,1)
+            }
+        }
+    }
+    return source;
+}
