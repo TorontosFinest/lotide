f9ee53502436fd30c19d2ab6889f43f2bdf5c7fb
TorontosFinest <abdialicanada20@gmail.com>
2021-11-24 21:35:16 +0000

    letterPositions.js file

---
+++ letterPositions.js
@@ -0,0 +1,13 @@
+const letterPositions = function(sentence) {
+    const output = {};
+    for (let i = 0; i < sentence.length; i++) {
+      if (sentence[i] !== " ") {
+        if (output[sentence[i]]) {
+          output[sentence[i]].push(i);
+        } else {
+          output[sentence[i]] = [i]
+        }
+      }
+    }
+    return output;
+  }
