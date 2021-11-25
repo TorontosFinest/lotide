a803459e5ffb6d024bb45e58f1116488a231590b
TorontosFinest <abdialicanada20@gmail.com>
2021-11-25 01:15:04 +0000

    eqObjects.js

---
+++ eqObjects.js
@@ -0,0 +1,31 @@
+const eqObjects = function(obj1, obj2) {
+    let Arr1 = Object.keys(obj1);
+    let Arr2 = Object.keys(obj2);
+  
+    if (Arr1.length !== Arr2.length) {
+      return false;
+    }
+  
+    for (val of Arr1) {
+      if (Array.isArray(obj1[val]) || Array.isArray(obj2[val])) {
+        if (!eqArrays(obj1[val], obj2[val])) {
+          return false;
+        }
+      } else if (obj1[val] === obj2[val]) {
+        return true;
+      }
+    }
+    return false;
+  };
+  
+  
+  var eqArrays = function(arr1, arr2) {
+  
+    for (var i = 0; arr1.length < i; i++) {
+      if (arr1[i] !== arr2[i]) {
+        return false;
+      }
+    }
+    return true;
+  };
+ 
