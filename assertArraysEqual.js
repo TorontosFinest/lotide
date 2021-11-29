bb68e00c9ddabc3c0dd3b873f6d98639fd39bfa2
TorontosFinest <abdialicanada20@gmail.com>
2021-11-29 21:36:14 +0000

    added package.json and index.js

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,22 +1,11 @@
-var eqArrays = function(arr1, arr2) {
+const eqArrays = require("./eqArrays");
 
-    for (var i = 0; arr1.length < i; i++) {
-      if (arr1[i] !== arr2[i]) {
-        return false;
-      }
-    }
-    return true;
-  };
+const assertArraysEqual = function (array1, array2) {
+  if (eqArrays(array1, array2)) {
+    console.log(`✅✅✅  Assertion passed: ${array1} === ${array2}`);
+  } else {
+    console.log(`🛑🛑🛑  Assertion failed: ${array1} !== ${array2}`);
+  }
+};
 
-  const assertArraysEqual = function(array1, array2) {
-    if (eqArrays(array1, array2)) {
-      console.log(`✅✅✅  Assertion passed: ${array1} === ${array2}`);
-    } else {
-      console.log(`🛑🛑🛑  Assertion failed: ${array1} !== ${array2}`);
-    }
-  };
-
-
-
-  
-  
+module.exports = assertArraysEqual;
--- middle.js
+++ middle.js
@@ -1,28 +1,13 @@
-var eqArrays = function(arr1, arr2) {
+const middle = function (arr) {
+  if (arr.length <= 2) {
+    return [];
+  } else if (arr.length % 2 === 0) {
+    let mid1 = arr.length / 2 - 1;
+    let mid2 = arr.length / 2;
+    return [arr[mid1], arr[mid2]];
+  } else {
+    return [arr[Math.floor(arr.length / 2)]];
+  }
+};
 
-    for (var i = 0; arr1.length < i; i++) {
-      if (arr1[i] !== arr2[i]) {
-        return false;
-      }
-    }
-    return true;
-  };
-
-  const assertArraysEqual = function(array1, array2) {
-    if (eqArrays(array1, array2)) {
-      console.log(`✅✅✅  Assertion passed: ${array1} === ${array2}`);
-    } else {
-      console.log(`🛑🛑🛑  Assertion failed: ${array1} !== ${array2}`);
-    }
-  };
-  const middle = function(arr){
-    if(arr.length<=2){
-        return [];
-    }else if(arr.length %2 === 0){
-        let mid1 = arr.length/2-1;
-        let mid2 = arr.length/2;
-        return arr[mid1],arr[mid2];
-    }else{
-     return [arr[Math.floor(arr.length / 2)]];
-    }
- }
+module.exports = middle;
