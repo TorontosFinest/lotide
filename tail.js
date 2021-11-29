f0dcd0511ae1dec6d65e0024b87eb35fd5d21758
TorontosFinest <abdialicanada20@gmail.com>
2021-11-29 19:14:42 +0000

    refactored tail

--- tail.js
+++ tail.js
@@ -1,17 +1,6 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion passed.${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion failed. ${actual} !== ${expected}`);
-  }
-};
-
-const  tail = function(arr) {
+const tail = function (arr) {
   let b = arr.slice(1);
   return b;
 };
-  
-const result = (tail([5, 7, 6]));
-assertEqual(result.length, 2);
 
-
+module.exports = tail;
